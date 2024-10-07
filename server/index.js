def631550def399ce9a8f7e55cd5f1618c60e65d
malishubham025 <malishubham025@gmail.com>
2024-10-07 22:19:43 +0530

    backend vercel netlify3

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const cors = require('cors')
 const mongoose=require("mongoose");
 const { v4: uuidv4 } = require('uuid');
 var jwt = require('jsonwebtoken');
-app.use(cors({ origin:[ 'http://localhost:3000','https://stalwart-vacherin-9cef56.netlify.app'] }));
+app.use(cors({ 
+    origin:[ 'http://localhost:3000','https://stalwart-vacherin-9cef56.netlify.app'] ,
+    methods: ['GET', 'POST'],
+}));
 app.use(express.json());
 require('dotenv').config();
 console.log(process.env.user);
