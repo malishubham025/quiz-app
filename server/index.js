ede52754e0309f68a5a545b95622e909aa8f1e24
malishubham025 <malishubham025@gmail.com>
2024-10-07 22:27:38 +0530

    backend vercel netlify4

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ app.use(cors({
     origin:[ 'http://localhost:3000','https://stalwart-vacherin-9cef56.netlify.app'] ,
     methods: ['GET', 'POST'],
 }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all domains
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(express.json());
 require('dotenv').config();
 console.log(process.env.user);
